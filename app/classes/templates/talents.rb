29de7af0d2c8d7709db285385865f2b9aef2bc3d
Rafael Carrilho <rafael.carrilho@runrun.it>
2023-07-30 12:54:06 -0300

    Talents creation from seeds

---
+++ app/classes/templates/talents.rb
@@ -0,0 +1,11 @@
+module Templates
+  class Talents
+
+    def self.all
+      [
+        ACTOR,
+        ALERT
+      ]
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+GameClass.destroy_all
 Templates::GameClasses.all.each do |game_class|
   GameClass.create!(game_class)
 end
 
+Race.destroy_all
 Templates::Races.all.each do |race|
   Race.create!(race)
+end
+
+Talent.destroy_all
+Templates::Talents.all.each do |talent|
+  Talent.create!(talent)
 end
